5db89d755f12f8dde7b7f122ef681c8e92ef92a3
Emmanuel Benitez Ledesma <emmanuel.benitez@grupomarktel.com>
2023-12-13 00:21:53 +0100

    pinia listo

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,20 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <nav class="navbar bg-light">
+  <nav class="navbar bg-dark navbar-dark">
     <div class="container">
       <RouterLink class="navbar-brand" to="/">
         <img src="@/assets/logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
         PokeAPI</RouterLink>
         <div>
           <RouterLink class="btn btn-outline-primary me-2" to="/">Home</RouterLink>
-          <RouterLink class="btn btn-outline-primary" to="/pokemons">Pokemons</RouterLink>
+          <RouterLink class="btn btn-outline-primary me-2" to="/pokemons">Pokemons</RouterLink>
+          <RouterLink class="btn btn-outline-primary" to="/favoritos">Favoritos</RouterLink>
         </div>
     </div>
   </nav>
 
-  <div class="container">
+  <div class="container text-center">
     <RouterView />
   </div>
 </template>
